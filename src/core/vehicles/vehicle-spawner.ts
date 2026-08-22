d84cb52da2e46ad0144e4658fe82fe4b228f921a
Milos Milinkovic <112415637+milinkovicmilos@users.noreply.github.com>
2026-08-22 14:17:23 +0200

    feat: vehicle spawner

---
+++ src/core/vehicles/vehicle-spawner.ts
@@ -0,0 +1,51 @@
+import type { Pathfinder } from '@core/pathfinding/pathfinder';
+import { Vehicle } from './vehicle';
+import type { DestinationGenerator } from './destination-generator';
+import type { Lane } from '@core/map/lane';
+import { PathLocation } from '@core/pathfinding/pathlocation';
+
+export class VehicleSpawner {
+    private readonly lanes: readonly Lane[];
+    private readonly pathfinder: Pathfinder;
+    private readonly destinationGenerator: DestinationGenerator;
+
+    constructor(lanes: Lane[], pathfinder: Pathfinder, destinationGenerator: DestinationGenerator) {
+        this.lanes = lanes;
+        this.pathfinder = pathfinder;
+        this.destinationGenerator = destinationGenerator;
+    }
+
+    spawn(): Vehicle {
+        const start = this.generateStartLocation();
+
+        const destination = this.destinationGenerator.generate(start);
+
+        const path = this.pathfinder.findPath(start, destination);
+
+        if (path === null) {
+            throw new Error('Failed to find a path for spawned vehicle.');
+        }
+
+        return new Vehicle(path, this.getRandomSpeed());
+    }
+
+    private generateStartLocation(): PathLocation {
+        const lane = this.getRandomLane();
+
+        const distance = Math.random() * lane.getLength();
+
+        return new PathLocation(lane, distance);
+    }
+
+    private getRandomLane(): Lane {
+        const index = Math.floor(Math.random() * this.lanes.length);
+
+        return this.lanes[index];
+    }
+
+    private getRandomSpeed(): number {
+        // Temporary value until vehicle speed
+        // configuration is introduced.
+        return 10;
+    }
+}
