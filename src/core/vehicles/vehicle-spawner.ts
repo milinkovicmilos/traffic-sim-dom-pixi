6bcb99e959a638d2a14360709775b48b4e515139
Milos Milinkovic <112415637+milinkovicmilos@users.noreply.github.com>
2026-08-23 18:19:29 +0200

    cleanup: remove console logs

--- src/core/vehicles/vehicle-spawner.ts
+++ src/core/vehicles/vehicle-spawner.ts
@@ -3,20 +3,24 @@ import { Vehicle } from './vehicle';
 import type { DestinationGenerator } from './destination-generator';
 import type { Lane } from '@core/map/lane';
 import { PathLocation } from '@core/pathfinding/pathlocation';
+import type { VehicleConfig } from '@shared/config/vehicle-config';
 
 export class VehicleSpawner {
     private readonly lanes: readonly Lane[];
     private readonly pathfinder: Pathfinder;
     private readonly destinationGenerator: DestinationGenerator;
+    private readonly vehicleConfig: VehicleConfig;
 
     constructor(
         lanes: readonly Lane[],
         pathfinder: Pathfinder,
         destinationGenerator: DestinationGenerator,
+        vehicleConfig: VehicleConfig,
     ) {
         this.lanes = lanes;
         this.pathfinder = pathfinder;
         this.destinationGenerator = destinationGenerator;
+        this.vehicleConfig = vehicleConfig;
     }
 
     spawn(): Vehicle {
@@ -25,15 +29,12 @@ export class VehicleSpawner {
         const destination = this.destinationGenerator.generate(start);
 
         const path = this.pathfinder.findPath(start, destination);
-        console.log(this.pathfinder);
-        console.log(this.lanes);
-        console.log(destination);
 
         if (path === null) {
             throw new Error('Failed to find a path for spawned vehicle.');
         }
 
-        return new Vehicle(path, this.getRandomSpeed());
+        return new Vehicle(path, this.vehicleConfig.maxSpeed);
     }
 
     private generateStartLocation(): PathLocation {
@@ -48,11 +49,5 @@ export class VehicleSpawner {
         const index = Math.floor(Math.random() * this.lanes.length);
 
         return this.lanes[index];
-    }
-
-    private getRandomSpeed(): number {
-        // Temporary value until vehicle speed
-        // configuration is introduced.
-        return 10;
     }
 }
