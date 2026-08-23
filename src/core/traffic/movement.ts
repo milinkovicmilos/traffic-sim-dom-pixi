d6d482692b2e6f633f3239b626c2d8b7e11309bb
Milos Milinkovic <112415637+milinkovicmilos@users.noreply.github.com>
2026-08-23 18:37:40 +0200

    feat: check the movement validity and expose the node that the movement is being made on, simplify the traffic light sytem canProceed method

--- src/core/traffic/movement.ts
+++ src/core/traffic/movement.ts
@@ -1,10 +1,16 @@
 import type { Lane } from '@core/map/lane';
+import type { RoadNode } from '@core/map/road-node';
 
 export class Movement {
     private readonly incomingLane: Lane;
     private readonly outgoingLane: Lane;
 
     constructor(incomingLane: Lane, outgoingLane: Lane) {
+        // Check if the lanes start/end in the same node
+        if (incomingLane.getEndNode() !== outgoingLane.getStartNode()) {
+            throw new Error('Incoming and outgoing lanes must be a part of the same road node.');
+        }
+
         this.incomingLane = incomingLane;
         this.outgoingLane = outgoingLane;
     }
@@ -15,5 +21,12 @@ export class Movement {
 
     getOutgoingLane(): Lane {
         return this.outgoingLane;
+    }
+
+    /**
+     * Returns the node at which the lanes are intersecting
+     */
+    getNode(): RoadNode {
+        return this.incomingLane.getEndNode();
     }
 }
