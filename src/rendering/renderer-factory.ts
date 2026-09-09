571296ab32592cd1218d15b816182a266a2e6a42
Miloš Milinković <112415637+milinkovicmilos@users.noreply.github.com>
2026-09-09 21:41:26 +0200

    PixiJS - optimization, WebGPU support

--- src/rendering/renderer-factory.ts
+++ src/rendering/renderer-factory.ts
@@ -1,13 +1,15 @@
 import type { Renderer } from './renderer';
 
 import { DOMRenderer, type DOMRendererOptions } from './dom/dom-renderer';
+
 import { PixiRenderer, type PixiRendererOptions } from './pixi/pixi-renderer';
 
-export type RendererType = 'dom' | 'pixi';
+export type RendererType = 'dom' | 'pixi-webgl' | 'pixi-webgpu';
 
 export interface RendererOptions {
     dom: DOMRendererOptions;
-    pixi: PixiRendererOptions;
+    pixiWebgl: PixiRendererOptions;
+    pixiWebgpu: PixiRendererOptions;
 }
 
 export function createRenderer(type: RendererType, options: RendererOptions): Renderer {
@@ -15,8 +17,17 @@ export function createRenderer(type: RendererType, options: RendererOptions): Re
         case 'dom':
             return new DOMRenderer(options.dom);
 
-        case 'pixi':
-            return new PixiRenderer(options.pixi);
+        case 'pixi-webgl':
+            return new PixiRenderer({
+                ...options.pixiWebgl,
+                preference: 'webgl',
+            });
+
+        case 'pixi-webgpu':
+            return new PixiRenderer({
+                ...options.pixiWebgpu,
+                preference: 'webgpu',
+            });
 
         default:
             throw new Error(`Unsupported renderer: ${type}`);
